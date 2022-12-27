03f29b33316ca090b163a72656406fe111ed42a7
Luoxi Pan <l.panpax@gmail.com>
2022-12-27 16:02:27 +1100

    Format using prettier

--- board.js
+++ board.js
@@ -7,12 +7,12 @@ const COLUMNS = 8;
 
 function createBoard() {
     // note that this board array is referenced as board[Y-coord][X-coord]
-    const _board = []
+    const _board = [];
 
     // populate board with empty spaces (denoted by zeroes)
     for (let i = 0; i < ROWS; i++) {
         // note that board[i] refers to Y coordinates, i.e. rows
-        _board.push(Array(COLUMNS).fill('⬚'))
+        _board.push(Array(COLUMNS).fill('⬚'));
     }
 
     return _board;
@@ -22,7 +22,7 @@ function createBoard() {
 function isMoveLegal(move) {
     const x = move[0];
     const y = move[1];
-    if ( (x >= 0 && x < 8) && (y >= 0 && y < 8)) {
+    if (x >= 0 && x < 8 && y >= 0 && y < 8) {
         return true;
     }
     return false;
@@ -38,28 +38,28 @@ function listMoves(position) {
 
     // list of all possible knight moves
     const candidateMoves = [
-        [(x - 2), (y + 1)], // A1
-        [(x - 1), (y + 2)], // A2
-        [(x + 1), (y + 2)], // B1
-        [(x + 2), (y + 1)], // B2
-        [(x + 2), (y - 1)], // C1
-        [(x + 1), (y - 2)], // C2
-        [(x - 1), (y - 2)], // D1
-        [(x - 2), (y - 1)], // D2
+        [x - 2, y + 1], // A1
+        [x - 1, y + 2], // A2
+        [x + 1, y + 2], // B1
+        [x + 2, y + 1], // B2
+        [x + 2, y - 1], // C1
+        [x + 1, y - 2], // C2
+        [x - 1, y - 2], // D1
+        [x - 2, y - 1], // D2
     ];
 
     // run all possible move patterns from and all valid moves to array
     candidateMoves.forEach((move) => {
-        if(isMoveLegal(move)) {
+        if (isMoveLegal(move)) {
             validMoves.push(move);
         }
-    })
+    });
 
     return validMoves;
 }
 
 function arrayEqual(arrA, arrB) {
-    if ( (arrA[0] === arrB[0]) & arrA[1] === arrB[1]) {
+    if ((arrA[0] === arrB[0]) & (arrA[1] === arrB[1])) {
         return true;
     }
     return false;
@@ -69,8 +69,8 @@ function pathTrace(lastNode) {
     let node = lastNode;
     const path = [];
 
-    while(node !== null) {
-        path.push(node.position)
+    while (node !== null) {
+        path.push(node.position);
         node = node.parent;
     }
 
@@ -78,20 +78,19 @@ function pathTrace(lastNode) {
 }
 
 function visualiseTrace(path) {
-    const traceBoard = createBoard()
+    const traceBoard = createBoard();
     let moveNumber = 0;
 
-    for(let i = 0; i < path.length; i ++) {
+    for (let i = 0; i < path.length; i++) {
         const position = path[i];
 
-        if(i === 0) {
+        if (i === 0) {
             traceBoard[position[0]][position[1]] = 'S';
         } else {
             traceBoard[position[0]][position[1]] = moveNumber;
         }
 
         moveNumber += 1;
-
     }
 
     return traceBoard;
@@ -110,20 +109,22 @@ function findPath(start, destination) {
     while (queue.length > 0) {
         // dequeue
         const node = queue.shift();
-        
+
         visited.add(JSON.stringify(node.position));
 
         // record sequence of nodes explored on board
         board[node.position[0]][node.position[1]] = nodeNumber;
         nodeNumber += 1;
-        
+
         // if current position equals destination
-        if(arrayEqual(node.position, destination)) {
+        if (arrayEqual(node.position, destination)) {
             console.log(`Destination found: ${destination}`);
             board[node.position[0]][node.position[1]] = 'D';
 
             const path = pathTrace(node);
-            console.log(`Path found in ${path.length - 1} moves! Here is your path: `); // - 1 to discount starting position
+            console.log(
+                `Path found in ${path.length - 1} moves! Here is your path: `
+            ); // - 1 to discount starting position
             console.log(path);
 
             const movePathBoard = visualiseTrace(path);
@@ -135,16 +136,15 @@ function findPath(start, destination) {
         // enqueue (expand current node by listing valid moves)
         const candidateMoves = listMoves(node.position);
 
-        if(candidateMoves) {
-            candidateMoves.forEach( (move) => {
+        if (candidateMoves) {
+            candidateMoves.forEach((move) => {
                 // check that position hasn't already been seen before
-                if(visited.has(JSON.stringify(move))) {
-
+                if (visited.has(JSON.stringify(move))) {
                 } else {
                     let nodePosition = new Node(move);
                     nodePosition.parent = node;
                     // add new potential moves to queue
-                    queue.push(nodePosition)
+                    queue.push(nodePosition);
                 }
             });
         }
